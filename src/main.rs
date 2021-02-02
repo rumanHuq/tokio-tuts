bf205b8282950877492c6ebd6f6ae78f79c41f4b
Majedul Hoque <rumanbsl@gmail.com>
2021-02-02 16:48:21 +0200

    chapter 5 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+	io::{self, AsyncReadExt, AsyncWriteExt},
+	net::TcpListener,
+	spawn,
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+	let listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+	loop {
+		let (mut socket, _) = listener.accept().await?;
+
+		spawn(async move {
+			let mut buf = vec![0; 1204];
+
+			loop {
+				match socket.read(&mut buf).await {
+					Ok(0) => return,
+					Ok(n) => {
+						if socket.write_all(&buf[..n]).await.is_err() {
+							return;
+						}
+					}
+					Err(_) => return,
+				}
+			}
+		});
+	}
 }
